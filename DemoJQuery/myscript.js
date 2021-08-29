03c67303c647b33ed5222853af22c31ba5e0dc86
ClaudeGrossl <onari.grossl@globo.com>
2021-08-29 18:25:49 -0300

    mm

--- DemoJQuery/myscript.js
+++ DemoJQuery/myscript.js
@@ -3,3 +3,10 @@ window.onload = function() {
     alert( "welcome" );
  
 };
+ 
+$( document ).ready(function() {
+    $( "a" ).click(function( event ) {
+        alert( "The link will no longer take you to jquery.com" );
+        event.preventDefault();
+    });
+});
