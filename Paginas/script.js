a6ea34f695e542feaaef2d74b74a9ebd10eed56f
ClaudeGrossl <onari.grossl@globo.com>
2021-10-21 05:36:59 -0300

    Update script.js

--- Paginas/script.js
+++ Paginas/script.js
@@ -1,19 +1,3 @@
-{
-const display27610 = document.getElementById("display");
-
-// conver number num to string, pad 0 in front to make it 2 digits
-const padNum = ((num) => num.toString(). padStart (2 , "0"));
-
-const updateDisplay = (() => {
-    const dd = new Date();
-    const hh = dd.getHours();
-    const mm = dd.getMinutes();
-    const ss = dd.getSeconds();
-    display27610.textContent = `${padNum(hh)}:${padNum(mm)}:${padNum(ss)}`;
-});
-
-//setInterval(updateDisplay, 1000);
-}
 
 
 function newScript() {
